436f10f360b08f2c332df5a1dad56b5b7229b57e
milesford99 <milesford@Miless-MacBook-Pro.local>
2020-03-15 19:51:48 -0400

    3rd commit

--- pythoncode.py
+++ pythoncode.py
@@ -1,3 +1,8 @@
 print ("Hello Github")
 var=input ("What is your name?")
 print ("Hello, " + var)
+age=input ("What is your age?")
+
+if age< "21":
+    print ("You're too young to drink!")
+else: print ("Go pick me up a six pack!")
