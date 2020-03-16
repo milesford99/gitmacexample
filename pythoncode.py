8d74a8125ceb91229ab7d73356393c2209b616ff
milesford99 <milesford@Miless-MacBook-Pro.local>
2020-03-15 22:35:11 -0400

    commit about beer questions

--- pythoncode.py
+++ pythoncode.py
@@ -6,3 +6,10 @@ age=input ("What is your age?")
 if age< "21":
     print ("You're too young to drink!")
 else: print ("Go pick me up a six pack!")
+
+print ("But I'm not sure what kind I want...")
+beer=input ("What kind do you like?")
+
+if beer == "pbr" or beer == "pabst":
+    print ("Domestics huh?")
+else: print ("I don't think I've ever heard of " + beer)
